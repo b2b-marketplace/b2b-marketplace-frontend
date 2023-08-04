c1f6433f912106d92eedd98ccc1e5a44715b0c9d
Dmitriy Tipsin <tipsin.dm@yandex.ru>
2023-08-04 17:23:22 +0300

    feat:  Установлена максимальная ширина блока в 1440px, добавлены иконки star и cgat-fill

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import './Main.scss';
-import IconChatFill from '../Icon/Icon_chat-fill';
 const Main = () => {
   return <div className="main">
-    <IconChatFill />
   </div>;
 };
 
