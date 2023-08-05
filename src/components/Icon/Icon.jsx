fc8a91db22098a3f81f17ea4a07111bbfc74a924
Dmitriy Tipsin <tipsin.dm@yandex.ru>
2023-08-05 14:48:22 +0300

    fix: Добавлено условие на проверку присутствия названия класса в компонент Icon

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './Icon.scss';
 
 const Icon = ({ children, className, ...props }) => {
+
   const localClassName = (className === "undefined") ? '' : className;
+
   return (
     <span {...props} className={`icon ${localClassName}`}>
       {children}
