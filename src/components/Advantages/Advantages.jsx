ae864127547640d12dba42cb8043cbc7000add87
Dmitriy Tipsin <tipsin.dm@yandex.ru>
2023-08-06 09:52:47 +0300

    fix: Исправлено название классов

---
+++ src/components/Advantages/Advantages.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Advantages = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Advantages;
---
+++ src/components/TagsNavigationHorizontal/TagsNavigationHorizontal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TagsNavigationHorizontal = () => {
+  return (
+    <ul className="tags-navigation-horizontal">
+      <li className="tags-navigation-horizontal__item">
+        <a className="tags-navigation-horizontal__link" href="#">Оптовые поставщики обуви</a>
+      </li>
+      <li className="tags-navigation-horizontal__item">
+        <a className="tags-navigation-horizontal__link" href="#">Китайские поставщики</a>
+      </li>
+      <li className="tags-navigation-horizontal__item">
+        <a className="tags-navigation-horizontal__link" href="#">Оптовые поставщики обуви</a>
+      </li>
+    </ul>
+  );
+};
+
+export default TagsNavigationHorizontal;
