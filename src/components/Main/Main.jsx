2d4fac6e14b9dce756b4b148c909dfd66f66b4a2
Дмитрий Типсин <113538427+TIPDMR@users.noreply.github.com>
2023-08-06 10:17:35 +0300

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import './Main.scss';
 import TagsNavigationHorizontal from "../TagsNavigationHorizontal/TagsNavigationHorizontal";
 const Main = () => {
-  return <div className="main">
-    <TagsNavigationHorizontal />
-  </div>;
+  return <div className="main"></div>;
 };
 
 export default Main;
