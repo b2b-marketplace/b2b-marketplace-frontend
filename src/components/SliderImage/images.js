61294722354664656826be0bee9b86f5fdc1171b
Mikhail Sulim <mika-1984@yandex.ru>
2023-08-07 12:35:36 +0300

    feat: создать комонент слайдера для карточки товара на главной странице

---
+++ src/components/SliderImage/images.js
@@ -0,0 +1,7 @@
+import img1 from './1.jpg';
+import img2 from './2.jpg';
+import img3 from './3.jpg';
+import img4 from './4.jpg';
+import img5 from './5.jpg';
+
+export const images = [img1, img2, img3, img4, img5];
