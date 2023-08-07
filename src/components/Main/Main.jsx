8cf46e2945c4a8a9a8e2ea6bfeaf79b62974988b
Mikhail Sulim <mika-1984@yandex.ru>
2023-08-07 21:27:10 +0300

    refactor: отредактировать параметры иконок и изменить структуру фалов слайдера

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Main.scss';
+import ProductCard from '../ProductCard/ProductCard';
 
 const Main = () => {
   return <div className="main">
-    
+    <ProductCard/>
   </div>;
 };
 
