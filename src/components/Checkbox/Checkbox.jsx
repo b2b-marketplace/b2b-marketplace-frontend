c65309a10a668a3933d963cd8047ac06c88740e6
Дмитрий Типсин <tipsin.dm@yandex.ru>
2023-08-12 19:20:49 +0300

    fix: Убран пробел

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import "./Checkbox.scss";
 
 const Checkbox = ({ className, children, name, value, ...props }) => {
+
   return (
-    <label className={`checkbox ${className ? className : ''}`}>
+    <label {...props} className={`checkbox ${className ? className : ''}`}>
       <input className="checkbox__input" type="checkbox" name={name} value={value}/>
       <div className="checkbox__span"></div>
     </label>
