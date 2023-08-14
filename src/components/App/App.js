b8532ec0f10e4c7e5546c5db0af4fb3f675f72d3
Petr Voevodin <demonle01@yandex.ru>
2023-08-14 11:33:31 +0300

    поправки

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,16 +2,11 @@ import './App.scss';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
-import AccountProductAdd from '../Account/AccountProductAdd/AccountProductAdd';
-
 
 function App() {
   return (
     <div className="app">
-      
       <Header />
-      <AccountProductAdd/>
-     
       <Main />
       <Footer />
     </div>
