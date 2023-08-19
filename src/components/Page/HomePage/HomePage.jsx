a0eb869e22831b93ddde8e677cc85d889ac51221
Дмитрий Типсин <tipsin.dm@yandex.ru>
2023-08-19 16:43:59 +0300

    fix: Добавлен Header и Promo

--- src/components/Page/HomePage/HomePage.jsx
+++ src/components/Page/HomePage/HomePage.jsx
@@ -5,15 +5,17 @@ import ProductCardContainer from "../../ProductCardContainer/ProductCardContaine
 import { PRODUCTS } from "../../../utils/productCardExample.js";
 import Banner from "../../Banner/Banner";
 import PromoRegistration from "../../PromoRegistration/PromoRegistration";
+import Promo from "../../Promo/Promo";
 
 const HomePage = () => {
   return (
     <div className="home-page">
+      <Promo/>
       <PlatformBenefits className="home-page__platform-benefits"/>
       <ProductCardContainer title="Новые товары" products={PRODUCTS} className="home-page__cards-container"/>
       <Banner className="home-page__banner"/>
       <ProductCardContainer title="Новые товары" products={PRODUCTS} className="home-page__cards-container"/>
-      <PromoRegistration className="home-page__promo-registration" />
+      <PromoRegistration className="home-page__promo-registration"/>
     </div>
   );
 };
