53c1b8cbdc985e4817b9a64f0853e245b8b7a95d
Petr Voevodin <demonle01@yandex.ru>
2023-08-20 11:04:46 +0300

    исправлены стили в компонентах Account

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,27 @@
 import './App.scss';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import HomePage from '../Page/HomePage/HomePage';
-import { Route, Routes } from 'react-router-dom';
-import AccountPage from '../Page/AccountPage/AccountPage';
-import AccountSellerProductAdd from '../Page/AccountPage/AccountSellerProductAdd/AccountSellerProductAdd';
-import React from 'react';
-import AccountBuyerPurchase from '../Page/AccountPage/AccountBuyerPurchase/AccountBuyerPurchase';
+import HomePage from "../Page/HomePage/HomePage";
+import { Route, Routes } from "react-router-dom";
+import AccountPage from "../Page/AccountPage/AccountPage";
+import AccountSellerProductAdd from "../Page/AccountPage/AccountSellerProductAdd/AccountSellerProductAdd";
+import React from "react";
+import AccountBuyerPurchase from "../Page/AccountPage/AccountBuyerPurchase/AccountBuyerPurchase";
 import AccountBuyerProfile from '../Page/AccountPage/AccountBuyerProfile/AccountBuyerProfile';
 
 function App() {
   return (
     <div className="app">
-      <Header />
+      <Header/>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/account" element={<AccountPage />}>
-          <Route path="profile" element={<AccountBuyerProfile />} />
-          <Route path="add" element={<AccountSellerProductAdd />} />
-          <Route path="orders" element={<AccountBuyerPurchase />} />
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/account" element={<AccountPage/>}>
+          <Route path="profile" element={<AccountBuyerProfile/>}/>
+          <Route path="add" element={<AccountSellerProductAdd/>}/>
+          <Route path="orders" element={<AccountBuyerPurchase/>}/>
         </Route>
       </Routes>
-      <Footer />
+      <Footer/>
     </div>
   );
 }
--- src/components/UI/Account/AccountTitle/AccountTitle.jsx
+++ src/components/UI/Account/AccountTitle/AccountTitle.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './AccountTitle.scss';
 
-const AccountTitle = ({ title, icon, name, logo }) => {
+const AccountTitle = ({ title, icon, name, logoSrc }) => {
   return (
     <section className="account-title">
       <div className="account-title__conteiner">
         <div className="account-title__conteiner-logo">
-          <img className='account-title__logo' src={logo} alt="Логотоп." />
+          <img className='account-title__logo' src={logoSrc} alt="Логотоп." />
         </div>
 
         <div className="account-title__about">
