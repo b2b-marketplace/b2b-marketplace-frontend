c53758b5afa3636cf9f4afa4e268240c38513b8a
Alexeev Eugene <alekseev.evgeniy.wd@yandex.ru>
2023-08-20 11:04:06 +0300

    add: create popup buttons component

--- src/components/AuthPopup/PopupButtons/PopupButtons.jsx
+++ src/components/AuthPopup/PopupButtons/PopupButtons.jsx
@@ -1,6 +1,11 @@
-const PopupButtons = () => {
+import { Button } from '../../UI/Button/Button';
+
+const PopupButtons = ({ className, onClick, value1, value2, buttonText1, buttonText2, primary1, primary2 }) => {
   return (
-    <></>
+    <div className={`popup__buttons${className ? ` ${className}` : ''}`}>
+      <Button primary={primary1} value={value1} onClick={onClick} type="button" size="xxxxl">{buttonText1}</Button>
+      <Button primary={primary2} value={value2} onClick={onClick} type="button" size="xxxxl">{buttonText2}</Button>
+    </div>
   );
 };
 
