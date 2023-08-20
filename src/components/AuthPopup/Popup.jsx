a6f4ebce763f6c00718b3e969aa827b416a30a94
Alexeev Eugene <alekseev.evgeniy.wd@yandex.ru>
2023-08-20 11:02:50 +0300

    feat: close popup by event

--- src/components/AuthPopup/Popup.jsx
+++ src/components/AuthPopup/Popup.jsx
@@ -1,10 +1,31 @@
+import { useEffect } from 'react';
 import './Popup.scss';
 import IconClose from '../UI/Icon/IconClose';
 
 const Popup = ({ isOpen, onClose, children }) => {
   const isOpenPopupTypeClass = `auth${isOpen ? ' auth_opened' : ''}`;
+  const closeByEvent = (event) => {
+    if(event.key === 'Escape' || event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+
+    const closeByEvent = (event) => {
+      if(event.key === 'Escape' || event.target === event.currentTarget) {
+        onClose();
+      }
+    };
+    document.addEventListener('keyup', closeByEvent);
+
+    return () => {
+      document.removeEventListener('keyup', closeByEvent);
+    };
+  }, []);
+  
   return (
-    <section className={isOpenPopupTypeClass}>
+    <section className={isOpenPopupTypeClass} onClick={closeByEvent}>
       <div className="popup">
         <button type="button" onClick={onClose} className="popup__close">
           <IconClose className="popup__close-icon" />
