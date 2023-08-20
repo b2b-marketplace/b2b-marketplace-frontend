098d02b843036923ef50aff75761af01292a4c5c
Дмитрий Типсин <tipsin.dm@yandex.ru>
2023-08-20 22:20:26 +0300

    feat: Компонент личного кабинета списка закупок

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,25 +2,30 @@ import './App.scss';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import HomePage from "../Page/HomePage/HomePage";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import AccountPage from "../Page/AccountPage/AccountPage";
 import AccountSellerProductAdd from "../Page/AccountPage/AccountSellerProductAdd/AccountSellerProductAdd";
 import React from "react";
-import AccountBuyerPurchase from "../Page/AccountPage/AccountBuyerPurchase/AccountBuyerPurchase";
+import AccountBuyerOrders from "../Page/AccountPage/AccountBuyerOrders/AccountBuyerOrders";
 import AccountBuyerProfile from '../Page/AccountPage/AccountBuyerProfile/AccountBuyerProfile';
 import BasketPage from "../Page/BasketPage/BasketPage";
+import AccountBuyerOrderList from "../Account/AccountBuyerOrdersList/AccountBuyerOrderList";
 
 function App() {
   return (
     <div className="app">
       <Header/>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/basket" element={<BasketPage/>}/>
+        <Route path="/" element={<HomePage/>} exact/>
+        <Route path="/basket" element={<BasketPage/>} exact/>
         <Route path="/account" element={<AccountPage/>}>
-          <Route path="profile" element={<AccountBuyerProfile/>}/>
-          <Route path="add" element={<AccountSellerProductAdd/>}/>
-          <Route path="orders" element={<AccountBuyerPurchase/>}/>
+          <Route index path="profile" element={<AccountBuyerProfile/>} exact/>
+          <Route path="product/add" element={<AccountSellerProductAdd/>} exact/>
+          <Route path="orders" element={<AccountBuyerOrders/>}>
+            <Route index element={<AccountBuyerOrderList/>} exact/>
+            <Route path=":filter" element={<AccountBuyerOrderList/>}/>
+          </Route>
+          <Route index element={<Navigate to="profile" />} />
         </Route>
       </Routes>
       <Footer/>
