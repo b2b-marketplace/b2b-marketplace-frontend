311aad231e862f1d3230b7a23dc4151551b3a231
Alexeev Eugene <alekseev.evgeniy.wd@yandex.ru>
2023-08-20 18:18:38 +0300

    reafact: refact next button components

--- src/components/AuthPopup/NextButton/NextButton.jsx
+++ src/components/AuthPopup/NextButton/NextButton.jsx
@@ -2,7 +2,7 @@ import IconNext from "../../UI/Icon/IconNext";
 
 const NextButton = ({ onNext }) => {
   return (
-    <button className='popup__button popup__next' onClick={onNext}>
+    <button className='popup__next' onClick={onNext}>
       <IconNext />
     </button>
   );
