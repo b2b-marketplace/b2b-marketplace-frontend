a6db86769a6425be49edb92805ccac17a10980bb
Alexeev Eugene <alekseev.evgeniy.wd@yandex.ru>
2023-08-20 18:17:43 +0300

    featL second step company registration

--- src/components/AuthPopup/RegisterPopup/RegistrationSecondStep/RegistrationSecondStep.jsx
+++ src/components/AuthPopup/RegisterPopup/RegistrationSecondStep/RegistrationSecondStep.jsx
@@ -1,6 +1,61 @@
-const RegistrationSecondStep = () => {
+import Input from "../../Input/Input";
+import NextButton from "../../NextButton/NextButton";
+
+const RegistrationSecondStep = ({ onNext }) => {
   return (
-    <></>
+    <>
+      <h2 className="popup__title">Укажите данные организации</h2>
+      <Input
+        name="company"
+        type="text"
+        placeholder="ОАО «Компания»"
+        size="l"
+        text="Юридическое название"
+      />
+      <Input
+        name="tin"
+        type="number"
+        placeholder="ИНН"
+        size="l"
+        text="Без пробелов"
+      />
+      {/* <Input
+        name="adress"
+        type="text"
+        placeholder="Край/область"
+        size="l"
+        text="Выберите свой из списка"
+      /> */}
+      <Input
+        name="adress"
+        type="select"
+        placeholder="Край/область"
+        size="l"
+        text="Выберите свой из списка"
+      >
+        <option value="">Край/область</option>
+        <option value="1">Московская Область</option>
+        <option value="2">Ленинградская область</option>
+      </Input>
+
+      <div className="popup__inputs">
+        <Input
+          name="email"
+          type="email"
+          placeholder="Почта"
+          size="s"
+          text=""
+        />
+        <Input
+          name="tel"
+          type="tel"
+          placeholder="+7"
+          size="s"
+          text="Без дефисов и пробелов"
+        />
+      </div>
+      <NextButton onNext={onNext} />
+    </>
   );
 };
 
