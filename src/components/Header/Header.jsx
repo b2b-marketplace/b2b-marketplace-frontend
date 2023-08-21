3089548233441f7288218cd1c0cf162cdf80fccd
Дмитрий Типсин <tipsin.dm@yandex.ru>
2023-08-21 16:17:14 +0300

    fix: При нажатии на логотип, ведет на главную страницу

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,16 +3,17 @@ import './Header.scss';
 import IconPosition from '../../components/UI/Icon/Icon_position';
 import IconBoxRu from '../../components/UI/Icon/Icon_boxRu';
 import Catalog from "../Catalog/Catalog";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header__main">
-        <div className="header__container-logo">
-          <IconBoxRu />
-        </div>
+        <Link to="/" className="header__container-logo">
+          <IconBoxRu/>
+        </Link>
         <div className="header__search">
-          <input className="header__search-input" placeholder="Введите запрос" />
+          <input className="header__search-input" placeholder="Введите запрос"/>
           <button type="button" className="header__search-button">
             Найти
           </button>
@@ -22,7 +23,7 @@ const Header = () => {
           <span className="header__location-text">Санкт&#8209;Петербург</span>
         </button>
       </div>
-      <Catalog />
+      <Catalog/>
     </header>
   );
 };
