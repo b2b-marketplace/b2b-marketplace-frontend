fdf2e086db749af13f9fec3177a9970605eb0eaf
Дмитрий Типсин <tipsin.dm@yandex.ru>
2023-08-22 19:33:53 +0300

    fix: Добавлен HashRouter для работы роутов на gh-pages

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './components/App/App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      {/*<HashRouter>*/}
+      <App/>
+      {/*</HashRouter>*/}
     </BrowserRouter>
   </React.StrictMode>
 );
