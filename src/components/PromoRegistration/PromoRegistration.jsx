d740ec73115d424d251a67768da49bf6b077d6f2
Petr Voevodin <demonle01@yandex.ru>
2023-08-26 14:55:36 +0300

    меняет стили promo registration

--- src/components/PromoRegistration/PromoRegistration.jsx
+++ src/components/PromoRegistration/PromoRegistration.jsx
@@ -1,30 +1,27 @@
 import React from 'react';
 import './PromoRegistration.scss';
-import logo from '../../images/logo-registration.svg';
+import logo from '../../images/photo_promo-registration.jpg';
 import { Button } from '../UI/Button/Button';
-import ButtonScrollUp from '../ButtonScrollUp/ButtonScrollUp';
 
 const PromoRegistration = ({ className }) => {
   return (
     <section className={`promo-registration ${className ? className : ''}`}>
       <div className="promo-registration__conteiner">
-        <div>
-          <h2 className="promo-registration__title">Хотите закупаться дешевле?</h2>
+        <div className="promo-registration__control">
+          <h2 className="promo-registration__title">Покупайте дешевле!</h2>
           <p className="promo-registration__text">
             Зарегистрируйтесь на нашем портале и найдите новых клиентов, заказчиков и партнёров!
           </p>
-          <div className="promo-registration__button">
-            <Button size="xxl" primary={false} border={true}>
-              Зарегистрироваться
-            </Button>
-            <Button size="xxl" primary={true} border={true}>
-              Войти
-            </Button>
-          </div>
+
+          <Button size="l" primary={false} border={true}>
+            Зарегистрироваться
+          </Button>
+          <Button size="l" primary={true} border={true}>
+            Войти
+          </Button>
         </div>
         <img className="promo-registration__logo" src={logo} alt="Логотип." />
       </div>
-      <ButtonScrollUp />
     </section>
   );
 };
