e1df6a4af8abb768fbf6e066239240df52564fef
Petr Voevodin <demonle01@yandex.ru>
2023-08-28 22:10:51 +0300

    меняет стили header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,28 +2,30 @@ import React from 'react';
 import './Header.scss';
 import IconPosition from '../../components/UI/Icon/Icon_position';
 import IconBoxRu from '../../components/UI/Icon/Icon_boxRu';
-import Catalog from "../Catalog/Catalog";
-import { Link } from "react-router-dom";
+import Catalog from '../Catalog/Catalog';
+import { Link } from 'react-router-dom';
+import IconSearch from '../UI/Icon/Icon_search';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header__main">
         <Link to="/" className="header__container-logo">
-          <IconBoxRu/>
+          <IconBoxRu />
+          BOX.RU
         </Link>
         <div className="header__search">
-          <input className="header__search-input" placeholder="Введите запрос"/>
-          <button type="button" className="header__search-button">
-            Найти
+          <input className="header__input" placeholder="Введите текст" />
+          <button type="button" className="header__button">
+            <IconSearch className={'header__button-icon'}/>
           </button>
         </div>
         <button type="button" className="header__location">
-          <IconPosition className={'header__location-icon'}></IconPosition>
-          <span className="header__location-text">Санкт&#8209;Петербург</span>
+          <IconPosition className={'header__location-icon'}/>
+          <span className="header__location-text">Санкт-Петербург</span>
         </button>
       </div>
-      <Catalog/>
+      <Catalog />
     </header>
   );
 };
--- src/components/UI/Icon/Icon_scales.jsx
+++ src/components/UI/Icon/Icon_scales.jsx
@@ -4,10 +4,42 @@ import Icon from './Icon';
 const IconScales = ({ className, ...props }) => {
   return (
     <Icon {...props} className={`${className}`}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-        <rect x="24" y="9.33337" width="5.33333" height="17.3333" rx="1" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" />
-        <rect x="13.333" y="17.3334" width="5.33333" height="9.33333" rx="1" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" />
-        <rect x="2.66699" y="12" width="5.33333" height="14.6667" rx="1" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="33"
+        height="32"
+        viewBox="0 0 33 32"
+        fill="none">
+        <rect
+          x="24.667"
+          y="9.33325"
+          width="5.33333"
+          height="17.3333"
+          rx="1"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linejoin="round"
+        />
+        <rect
+          x="14"
+          y="17.3333"
+          width="5.33333"
+          height="9.33333"
+          rx="1"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linejoin="round"
+        />
+        <rect
+          x="3.33398"
+          y="12"
+          width="5.33333"
+          height="14.6667"
+          rx="1"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linejoin="round"
+        />
       </svg>
     </Icon>
   );
