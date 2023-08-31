88050b3dddddea432d17ad2a5835fc4509c2189e
Alexeev Eugene <alekseev.evgeniy.wd@yandex.ru>
2023-08-31 16:30:40 +0300

    feat: controle the checkbox

--- src/components/AuthPopup/LabelCheckbox/LabelCheckbox.jsx
+++ src/components/AuthPopup/LabelCheckbox/LabelCheckbox.jsx
@@ -1,8 +1,8 @@
-const LabelCheckbox = ({ id, text }) => {
+const LabelCheckbox = ({ id, text, isCheck, onCheck }) => {
   return (
 
     <label className="popup__label" htmlFor={id}>
-      <input id={id} type="checkbox" className={`popup__checkbox popup__checkbox_type${id}`} />
+      <input id={id} onChange={onCheck} checked={isCheck} type="checkbox" className={`popup__checkbox popup__checkbox_type${id}`} />
       <span className="popup__checkbox-span">{text}</span>
     </label>
   );
