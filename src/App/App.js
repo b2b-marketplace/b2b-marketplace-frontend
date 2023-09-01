a61217ece8ea8f70e8680491858b83487ef67204
Дмитрий Типсин <tipsin.dm@yandex.ru>
2023-09-01 09:48:34 +0300

    ref: Рефакторинг папок

--- src/App/App.js
+++ src/App/App.js
@@ -14,6 +14,7 @@ import ProductPage from "../Pages/ProductPage/ProductPage";
 import { PRODUCTS } from '../utils/productCardExample';
 
 function App() {
+
   return (
     <div className="app">
       <Header/>
