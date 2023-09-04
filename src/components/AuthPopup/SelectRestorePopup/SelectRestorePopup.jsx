17123aa8a19b687356827a62f5349aa6f5ace2c0
Alexeev Eugene <alekseev.evgeniy.wd@yandex.ru>
2023-09-05 00:18:16 +0300

    fix: fix select restore popup

--- src/components/AuthPopup/SelectRestorePopup/SelectRestorePopup.jsx
+++ src/components/AuthPopup/SelectRestorePopup/SelectRestorePopup.jsx
@@ -1,13 +1,12 @@
-import Form from "../Form/Form";
 import Popup from "../Popup";
 import PopupButtons from "../PopupButtons/PopupButtons";
 
-const SelectRestorePopup = ({ isOpen, onClose, onSubmit, onSelectType }) => {
+const SelectRestorePopup = ({ isOpen, onClose, onSelectType }) => {
 
   return (
     <Popup isOpen={isOpen} onClose={onClose}>
       <div className="popup__contaoner">
-        <h2 className="popup__title">Выберите другой способ входа</h2>
+        <h2 className="popup__title popup__title_restore">Выберите другой способ входа</h2>
         <PopupButtons
           onClick={onSelectType}
           value1="tel"
