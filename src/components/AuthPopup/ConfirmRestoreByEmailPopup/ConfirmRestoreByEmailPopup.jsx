4d7a770e9d1d0c1e393dff63a8d81dfe98411e28
Alexeev Eugene <alekseev.evgeniy.wd@yandex.ru>
2023-09-07 17:22:41 +0300

    feat: add popup comp...

--- src/components/AuthPopup/ConfirmRestoreByEmailPopup/ConfirmRestoreByEmailPopup.jsx
+++ src/components/AuthPopup/ConfirmRestoreByEmailPopup/ConfirmRestoreByEmailPopup.jsx
@@ -1,5 +1,7 @@
 import Form from "../Form/Form";
 import Popup from "../Popup";
+import PopupButton from "../PopupButton/PopupButton";
+import PopupCompleteParagraph from "../PopupCompleteParagraph/PopupCompleteParagraph";
 
 const ConfirmRestoreByEmailPopup = ({ isOpen, onClose, onSubmit }) => {
   const handleSubmit = () => {
@@ -13,8 +15,14 @@ const ConfirmRestoreByEmailPopup = ({ isOpen, onClose, onSubmit }) => {
         className="popup__form"
         onSubmit={handleSubmit}
       >
-        <h2 className="popup__title">Укажите, кто вы</h2>
-
+        <h2 className="popup__title">Мы отправили на указанный адрес ссылку для сброса пароля</h2>
+        <PopupCompleteParagraph>Если не нашли письмо от нас, проверьте папку спам</PopupCompleteParagraph>
+        <PopupButton
+          type="button"
+          className="popup__button popup__underlined-text popup__underlined-text_type_resend"
+        >
+          Письмо не пришло, отправить повторно через 01:00
+        </PopupButton>
       </Form>
     </Popup>
   );
