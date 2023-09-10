1e1c6d2ebfebd0215c0184b54a82e9169eb98659
Дмитрий Типсин <113538427+TIPDMR@users.noreply.github.com>
2023-09-10 14:31:52 +0300

    Revert "Olya1"

--- src/Pages/AccountPage/AccountBuyerProfile/AccountBuyerProfile.jsx
+++ src/Pages/AccountPage/AccountBuyerProfile/AccountBuyerProfile.jsx
@@ -1,83 +1,58 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './AccountBuyerProfile.scss';
-import AccountTitle from "../../../components/UI/Account/AccountTitle/AccountTitle";
-import AccountInputField from "../../../components/UI/Account/AccountInputField/AccountInputField";
-import IconPhone from "../../../components/UI/Icon/Icon_phone";
-import IconMail from "../../../components/UI/Icon/Icon_mail";
-import { Button } from "../../../components/UI/Button/Button";
+import AccountTitle from '../../../components/UI/Account/AccountTitle/AccountTitle';
+import IconPencil from '../../../components/UI/Icon/Icon_pencil';
+import AccountInputField from '../../../components/UI/Account/AccountInputField/AccountInputField';
+import Tooltip from '../../../components/UI/Tooltip/Tooltip';
+import IconPhone from '../../../components/UI/Icon/Icon_phone';
+import IconMail from '../../../components/UI/Icon/Icon_mail';
+import IconInfoFil from '../../../components/UI/Icon/Icon_info_fill';
+import AccountDropDown from '../../../components/UI/Account/AccountDropDown/AccountDropDown';
 
 const AccountBuyerProfile = () => {
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [isDisadled, setIsDisadled] = useState(true);
- 
-  function editInfo(e) {
-    e.preventDefault();
-    setIsEditMode(true);
-    setIsDisadled(false);
-  }
-
-  function handleEdit(e) {
-    e.preventDefault();
-    setIsEditMode(false);
-    setIsDisadled(true);
-  }
-
-  function cancelEdit(e) {
-    e.preventDefault();
-    setIsEditMode(false);
-    setIsDisadled(true);
-  }
-
-  
   return (
     <section className="account-buyer-profile">
       <div className="account-buyer-profile__blok">
-        <AccountTitle name="ООО «Планета»" title="Покупатель" />
-        
-        <form className="account-buyer-profile__form">
-          
-          <div className='account-buyer-profile__title-group'>
-            <h2 className="account-buyer-profile__title">Данные покупателя</h2>
-            <p className="account-buyer-profile__info">Их видите только вы и служба поддержки B2Buy.ru</p>
-          </div>
-          <fieldset className="account-buyer-profile__field" disabled={isDisadled}>
-            <AccountInputField label="Счет" placeholder="7209176529 0000" type="number" isDisabled={isDisadled} />
-            <AccountInputField label="ИНН" placeholder="000000000 0000" type="number" isDisabled={isDisadled}/>
-            <AccountInputField label="Регион доставки" placeholder="Москва, Россия" type="text" isDisabled={isDisadled}/>
-          </fieldset>
+        <AccountTitle name="ООО «Компания»" title="Покупатель" icon={<IconPencil />} />
 
-          <div className='account-buyer-profile__title-group'>
+        <form className="account-buyer-profile__form">
+          <div className="account-buyer-profile__control">
+            <h2 className="account-buyer-profile__title">Данные покупателя</h2>
+            <div className="account-buyer-profile__field">
+              <AccountDropDown label="Способ оплаты" />
+              <AccountInputField label="Счет" placeholder="000000000 0000" type="number" />
+              <AccountInputField label="ИНН" placeholder="000000000 0000" type="number" />
+              <AccountInputField label="Регион доставки" placeholder="Город" type="text" />
+            </div>
+          </div>
+
+          <div className="account-buyer-profile__control">
             <h2 className="account-buyer-profile__title">
               Контакты
-              {/* <Tooltip
+              <Tooltip
                 position="right"
                 tooltipContent={
                   <>Эти данные видите только&nbsp;вы, они скрыты от&nbsp;покупателей</>
                 }>
                 <IconInfoFil />
-              </Tooltip> */}
+              </Tooltip>
             </h2>
+            <div className="account-buyer-profile__field">
+              <AccountInputField
+                label="Почта"
+                placeholder="Почта"
+                type="email"
+                icon={<IconMail />}
+              />
+              <AccountInputField
+                label="Телефон"
+                placeholder="Телефон"
+                type="tel"
+                icon={<IconPhone />}
+              />
+            </div>
           </div>
-          <fieldset className="account-buyer-profile__field" disabled={isDisadled}>
-            <AccountInputField label="Почта" placeholder="E-mail" type="email" isDisabled={isDisadled} icon={<IconMail />}/>
-            <AccountInputField label="Телефон" placeholder="Телефон" type="tel" isDisabled={isDisadled} icon={<IconPhone />}/>
-          </fieldset>
-
-          {isEditMode ? (
-            <div className='account-buyer-profile__button-container'>
-              <Button size="xl" mode="secondary" type="submit" onClick={handleEdit}>Сохранить</Button>
-              <Button size="xl" mode="secondary" type="button" onClick={cancelEdit}>Отменить</Button>
-            </div>
-          ) : (
-            <div className='account-buyer-profile__button-container'>
-              <Button size="xl" mode="secondary" type="button" onClick={editInfo}>Редактировать</Button>
-            </div>
-          )
-          }
         </form>
-       
-        {/* <AccountPaymentInfo title="Способы оплаты" /> */}
-
       </div>
     </section>
   );
--- src/components/UI/Icon/Icon_mail.jsx
+++ src/components/UI/Icon/Icon_mail.jsx
@@ -4,24 +4,31 @@ import Icon from './Icon';
 const IconMail = ({ className, ...props }) => {
   return (
     <Icon {...props} className={`${className}`}>
-      <svg 
-        width="32" 
-        height="32" 
-        viewBox="0 0 32 32" 
-        fill="none" 
-        xmlns="http://www.w3.org/2000/svg">
-        <rect 
-          x="5.33337" 
-          y="8" 
-          width="21.3333" 
-          height="16" 
-          rx="2" 
-          stroke="#222222" 
-          strokeWidth="1.5"/>     
-        <path 
-          d="M5.33337 12L15.1056 16.8861C15.6687 17.1676 16.3314 17.1676 16.8945 16.8861L26.6667 12" 
-          stroke="#222222" 
-          strokeWidth="1.5"/>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 32 32"
+        fill="none">
+        <path
+          d="M5.33301 16C5.33301 14.4518 5.33301 13.6777 5.42803 13.029C6.00433 9.0946 9.09424 6.0047 13.0286 5.4284C13.6774 5.33337 14.4515 5.33337 15.9997 5.33337V5.33337C17.5479 5.33337 18.322 5.33337 18.9707 5.4284C22.9051 6.0047 25.995 9.0946 26.5713 13.029C26.6663 13.6777 26.6663 14.4518 26.6663 16V22.7879C26.6663 24.5559 26.6663 25.4399 26.1462 26.012C26.1035 26.059 26.0586 26.1039 26.0117 26.1466C25.4395 26.6667 24.5555 26.6667 22.7876 26.6667H15.9997C14.4515 26.6667 13.6774 26.6667 13.0286 26.5717C9.09424 25.9954 6.00433 22.9055 5.42803 18.9711C5.33301 18.3224 5.33301 17.5483 5.33301 16V16Z"
+          stroke="currentColor"
+          strokeWidth="1.5"
+        />
+        <path
+          d="M12 16L20 16"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M16 12L16 20"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
       </svg>
     </Icon>
   );
