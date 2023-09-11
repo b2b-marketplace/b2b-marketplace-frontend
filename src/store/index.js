05b57289c25bb58f98ba35603c8eb0dc2027c8ff
Дмитрий Типсин <tipsin.dm@yandex.ru>
2023-09-11 08:12:26 +0300

    feat: Добавлен redux-persist для восстановления товаров которые были добавлены в корзину пользователем из LocalStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,14 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { accountReducer } from "./slices/accountSlice";
 import { basketReducer } from "./slices/basketSlice";
@@ -18,8 +27,14 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: persistedReducer
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export const persister = persistStore(store);
+export const persistor = persistStore(store);
 export default store;
