bc271c95e522cc40277c6edaaa18f8efc3f94c56
Дмитрий Типсин <tipsin.dm@yandex.ru>
2023-09-11 07:38:43 +0300

    feat: Добавлен redux-persist для восстановления товаров которые были добавлены в корзину пользователем

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { accountReducer } from "./slices/accountSlice";
 import { basketReducer } from "./slices/basketSlice";
 
+const rootReducer = combineReducers({
+  account: accountReducer,
+  basket: basketReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['basket']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    account: accountReducer,
-    basket: basketReducer,
-  },
+  reducer: persistedReducer
 });
+
+export const persister = persistStore(store);
 export default store;
