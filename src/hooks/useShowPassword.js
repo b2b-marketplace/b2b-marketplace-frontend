990b6458d9876fe5e8f12f740f8b51649359bcd7
Alexeev Eugene <alekseev.evgeniy.wd@yandex.ru>
2023-09-15 03:06:00 +0300

    feat: add reset method

--- src/hooks/useShowPassword.js
+++ src/hooks/useShowPassword.js
@@ -3,7 +3,8 @@ import { useState } from "react";
 const useShowPassword = (initState = false) => {
   const [isShow, setIsShow] = useState(initState);
   const handleShow = () => setIsShow(!isShow);
-  return { isShow, handleShow };
+  const resetShow = (newState = false) => setIsShow(newState);
+  return { isShow, handleShow, resetShow };
 };
 
 export default useShowPassword;
