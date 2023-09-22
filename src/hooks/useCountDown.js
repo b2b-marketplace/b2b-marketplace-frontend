35fa94140507cee763fd8494f0095a60c936e2e2
Alexeev Eugene <alekseev.evgeniy.wd@yandex.ru>
2023-09-22 04:40:00 +0300

    feat: add metod to convert  time from seconds to minuts

--- src/hooks/useCountDown.js
+++ src/hooks/useCountDown.js
@@ -32,8 +32,14 @@ const useCountDown = (initialTime) => {
     setIsRunning(false);
     setTime(initialTime);
   };
+  
+  const formatTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  };
 
-  return { time, startTimer, resetTimer };
+  return { time, startTimer, resetTimer, formatTime };
 };
 
 export default useCountDown;
