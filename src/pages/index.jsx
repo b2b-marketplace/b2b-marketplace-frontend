c9af104e554ccd8a9fe5e2a563a6533c78c43c4f
Дмитрий Типсин <tipsin.dm@yandex.ru>
2023-10-23 20:03:33 +0300

    hotfix: Исправлено

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,6 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import React from 'react';
 
 import OrdersList from '../entities/orders/ui/OrdersList/OrdersList';
-import AccountPaymentInfo from '../components/UI/Account/AccountPaymentInfo/AccountPaymentInfo';
 import ProtectedRoutes from '../components/ProtectedRoutes/ProtectedRoutes';
 import Logout from '../components/Logout/Logout';
 import Favorites from '../components/Favorites/Favorites';
