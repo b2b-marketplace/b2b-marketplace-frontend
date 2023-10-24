49ae2cb2a293a4d5a9af551c4464f147d7ba80b4
Дмитрий Типсин <tipsin.dm@yandex.ru>
2023-10-24 17:57:28 +0300

    feat: Добавлены компоненты AppH1, AppH2, AppH3

--- src/widgets/account/ui/AccountHeader/AccountHeader.jsx
+++ src/widgets/account/ui/AccountHeader/AccountHeader.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import React from 'react';
-import PropTypes from 'prop-types';
 
+import { AppH3 } from '../../../../shared/ui/AppHeading';
 import { USER_ROLE_NAME } from '../../../../shared/config/constants';
 
 import noImage from '../../../../images/basket/Stub_132_128.jpg';
@@ -25,7 +25,7 @@ const AccountHeader = () => {
         <img className="account-header__logo" src={noImage} alt="Логотип" />
       </div>
       <div className="account-header__container">
-        <h3 className="account-header__name">{company.name}</h3>
+        <AppH3>{company.name}</AppH3>
         <p className="account-header__role">{userRoleName}</p>
       </div>
     </section>
