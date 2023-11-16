2f378605cea77c882fafedc59b61adcba4ab42f8
Дмитрий Типсин <tipsin.dm@yandex.ru>
2023-11-16 13:49:50 +0300

    chore: Добавлен пакет react-query

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 
@@ -10,14 +11,16 @@ import App from './app/App';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       {/* <HashRouter> */}
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
         </PersistGate>
       </Provider>
       {/* </HashRouter> */}
