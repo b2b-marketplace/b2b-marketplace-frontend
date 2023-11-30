897f20ee9af943f9da52cf7dc9c58fa134f53a06
VoevodinPetr <demonle01@yandex.ru>
2023-11-30 19:13:36 +0300

    компонент личного кабинета

--- src/shared/hooks/useInput.js
+++ src/shared/hooks/useInput.js
@@ -5,7 +5,7 @@ import { passwordLength, passwordRegEx, phoneRegEx } from '../lib/authConstatnts
 
 const useInput = (initValueParams) => {
   const [values, setValues] = useState(initValueParams);
-  const [errors, setErrors] = useState(initValueParams);
+  const [errors, setErrors] = useState({});
   const [isNotValidForm, setIsNotValidForm] = useState(true);
 
   const initDirtyStates = Object.keys(initValueParams).reduce((states, inputName) => {
@@ -26,10 +26,13 @@ const useInput = (initValueParams) => {
       return passwordRegEx.test(input.value);
     }
     if (input.type === 'tel') {
-      return phoneRegEx.test(input.value);
+      const isValidPhone = phoneRegEx.test(input.value);
+      const isLengthValid = input.value.length >= 10 && input.value.length <= 20;
+      return isValidPhone && isLengthValid;
     }
     return input.checkValidity();
   };
+
   const getCustomMessage = (input) => {
     if (input.type === 'email') {
       return 'Неверный формат почты';
@@ -41,26 +44,49 @@ const useInput = (initValueParams) => {
       return `От ${passwordLength} символов, латиница, цифры, символы`;
     }
     if (input.type === 'tel') {
-      return 'Длина телефона от 10 до 20 цифр';
+      const isValidPhone = phoneRegEx.test(input.value);
+      const isLengthValid = input.value.length >= 10 && input.value.length <= 20;
+
+      if (input.name === 'inn') {
+        return isLengthValid ? 'Введите минимум 10 цифр' : 'Введите от 10 до 20 цифр для телефона';
+      }
+
+      return isValidPhone && isLengthValid
+        ? input.validationMessage
+        : isLengthValid
+          ? 'Введите от 10 до 20 цифр для телефона'
+          : 'Введите минимум 10 цифр';
     }
+
     return input.validationMessage;
   };
 
   const handleChange = ({ target: input }) => {
-    if (!isDirtyInputs[input.name]) {
+    if (input.value === '') {
+      setValues((state) => ({
+        ...state,
+        [input.name]: '',
+      }));
+      return;
+    }
+
+    setValues((state) => ({
+      ...state,
+      [input.name]: input.type === 'checkbox' ? input.checked : input.value,
+    }));
+
+    setErrors((state) => ({
+      ...state,
+      [input.name]: checkValidity(input) ? input.validationMessage ?? '' : getCustomMessage(input),
+    }));
+
+    // Update isDirtyInputs only if the field has changed
+    if (!isDirtyInputs[input.name] && values[input.name] !== input.value) {
       setIsDirtyInputs((state) => ({
         ...state,
         [input.name]: true,
       }));
     }
-    setErrors((state) => ({
-      ...state,
-      [input.name]: checkValidity(input) ? input.validationMessage ?? '' : getCustomMessage(input),
-    }));
-    setValues((state) => ({
-      ...state,
-      [input.name]: input.type === 'checkbox' ? input.checked : input.value,
-    }));
   };
 
   useEffect(() => {
@@ -71,15 +97,12 @@ const useInput = (initValueParams) => {
     setIsNotValidForm(checkFormIsNotFullFilled || checkFormIsNotValid);
   }, [values, errors, isDirtyInputs]);
 
-  const resetValues = useCallback(
-    (newValues = initValueParams, newErrors = initValueParams, newNotIsVaild = false) => {
-      setValues(newValues);
-      setErrors(newErrors);
-      setIsNotValidForm(newNotIsVaild);
-      setIsDirtyInputs(initDirtyStates);
-    },
-    []
-  );
+  const resetValues = useCallback((newValues = initValueParams, newNotIsValid = false) => {
+    setValues(newValues);
+    setErrors({});
+    setIsNotValidForm(newNotIsValid);
+    setIsDirtyInputs(initDirtyStates);
+  }, []);
 
   return { values, errors, isNotValidForm, isDirtyInputs, handleChange, resetValues };
 };
--- src/shared/ui/AppInput/AppInput.jsx
+++ src/shared/ui/AppInput/AppInput.jsx
@@ -4,6 +4,23 @@ import clsx from 'clsx';
 
 import './AppInput.scss';
 
+/**
+ * Компонент для отображения поля ввода с различными настройками и стилями.
+ *
+ * @component
+ * @param {Object} props - Параметры компонента.
+ * @param {string} [props.className] - Дополнительные классы для настройки стилей компонента.
+ * @param {string} [props.mode='primary'] - Режим отображения поля ввода. Возможные значения: 'primary'.
+ * @param {string} [props.size='auto'] - Размер поля ввода. Возможные значения: 'auto'.
+ * @param {string} [props.type='text'] - Тип поля ввода. Возможные значения: 'text', 'number', 'password', 'email'.
+ * @param {string} [props.value] - Текущее значение поля ввода.
+ * @param {string} [props.placeholder] - Текст плейсхолдера для поля ввода.
+ * @param {boolean} [props.disabled=false] - Флаг, указывающий, является ли поле ввода неактивным.
+ * @param {function} [props.onClick] - Обработчик события клика на поле ввода.
+ * @param {function} [props.onChange] - Обработчик события изменения значения поля ввода.
+ * @returns {JSX.Element} - Возвращает JSX-элемент поля ввода.
+ */
+
 const AppInput = ({
   className,
   mode,
@@ -14,15 +31,23 @@ const AppInput = ({
   placeholder,
   onClick,
   onChange,
+  name,
+  required,
+  minLength,
+  maxLength,
 }) => {
   return (
     <input
+      name={name}
       type={type}
       value={value}
       onClick={onClick}
       onChange={onChange}
       placeholder={placeholder}
       disabled={disabled}
+      required={required}
+      minLength={minLength}
+      maxLength={maxLength}
       className={clsx(
         'app-input',
         mode && `app-input_${mode}`,
@@ -36,7 +61,7 @@ const AppInput = ({
 AppInput.propTypes = {
   mode: PropTypes.oneOf(['primary']),
   size: PropTypes.oneOf(['auto']),
-  type: PropTypes.oneOf(['text', 'number', 'password', 'email']),
+  type: PropTypes.oneOf(['text', 'number', 'password', 'email', 'tel']),
   className: PropTypes.string,
   disabled: PropTypes.bool,
   onClick: PropTypes.func,
